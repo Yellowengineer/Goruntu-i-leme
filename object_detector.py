87a67bcd091d424019a4ecafe023591e801e656b
Yellowengineer <137157563+Yellowengineer@users.noreply.github.com>
2024-05-21 08:39:19 +0300

    Create object_detector.py

---
+++ object_detector.py
@@ -0,0 +1,11 @@
+import cv2
+
+class HomogeneousBgDetector:
+    def __init__(self):
+        pass
+
+    def detect_objects(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
